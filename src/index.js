5c574914cffdb91abcf2ed2cc8e67d947a12423f
Manoel Eric Nascimento de Oliveira <manoel.oliveira@grendene.com.br>
2022-11-30 20:57:41 -0300

    alteração porta

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 // Observar passagem por variáveis de ambiente
-const PORT = process.env.PORT;
+const PORT = 3333;
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URL
 })
